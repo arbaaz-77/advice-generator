72ff3fdadcbf41990833cb6dd54592d5cd1b18c0
arbaaz-77 <43441056+arbaaz-77@users.noreply.github.com>
2022-07-06 12:24:59 +0300

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import "./App.css";
+import { useState } from "react";
+import dice from "./images/icon-dice.svg";
+import lineDesktop from "./images/pattern-divider-desktop.svg";
+import lineMobile from "./images/pattern-divider-mobile.svg";
 
 const url = "https://api.adviceslip.com/advice";
 
 function App() {
+  const [quote, setQuote] = useState(null);
+
+  const fetchData = () => {
+    fetch(url)
+      .then((rep) => rep.json())
+      .then((data) => {
+        setQuote(data.slip);
+      });
+  };
+
   return (
     <main>
-      <div class="card">
-        <p class="number">Advice #000</p>
-        <p class="advice">"Click the dice for random advice"</p>
-        <img src="../images/pattern-divider-desktop.svg" alt="" />
+      <div className="card">
+        <p className="number">
+          {quote ? `advice #${quote.id}` : "advice #000"}
+        </p>
+        <p className="advice">
+          {quote ? `"${quote.advice}"` : "Roll dice for advice :D"}
+        </p>
+        <img className="desktop" src={lineDesktop} alt="" />
+        <img className="mobile" src={lineMobile} alt="" />
       </div>
-      <button class="dice">
-        <img src="../images/icon-dice.svg" alt="" />
+      <button className="dice" onClick={fetchData}>
+        <img src={dice} alt="" />
       </button>
     </main>
   );
